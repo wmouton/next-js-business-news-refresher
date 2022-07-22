8eb9281c37f8b9d0019fe8b4bf65333db17be2e6
l33th <l33th@protonmail.ch>
2022-07-22 08:12:01 +0200

    chore: Header and styles

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import Image from 'next/image'
+import headerStyles from '../styles/Header.module.css'
+import logo from '../public/next.svg'
+
+const Header: FC = () => {
+	return (
+		<div>
+			<h1 className={headerStyles.title}>
+				<Image src={logo} />
+				<br/>
+				<span>Business</span>News
+			</h1>
+			<p className={headerStyles.description}>Keep up to date with the latest news in business.</p>
+		</div>
+	)
+}
+
+export default Header
